8ad3419532a5933a2fc93958390d4e22933a23b2
james-judd <jamesedtomjudd@gmail.com>
2023-02-27 20:39:39 +0000

    Spring app started: hello world @ localhost:8080

---
+++ app/src/main/java/com/chess/app/HelloController.java
@@ -0,0 +1,14 @@
+package com.chess.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+}
